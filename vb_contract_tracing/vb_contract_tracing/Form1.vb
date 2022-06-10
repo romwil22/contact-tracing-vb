f6fff24a783d9990841a19cc9c441aa03f39e615
romwil22 <romwil.pilapil@gmail.com>
2022-06-11 01:17:12 +0800

    modifying save button function, add new features

--- vb_contract_tracing/vb_contract_tracing/Form1.vb
+++ vb_contract_tracing/vb_contract_tracing/Form1.vb
@@ -19,19 +19,33 @@
 
     Private Sub saveButton_Click(sender As Object, e As EventArgs) Handles saveButton.Click
         Dim studentFile As System.IO.StreamWriter
-        ' text file create at \vb_contract_tracing\vb_contract_tracing\bin\Debug\<filename>.txt
-        studentFile = My.Computer.FileSystem.OpenTextFileWriter(lastNameBox.Text.ToUpper() + ".txt", True)
-        studentFile.WriteLine("FullName: " + lastNameBox.Text + ", " + nameBox.Text + " " + miBox.Text)
-        studentFile.WriteLine("Student Number: " + studentNumberBox.Text)
-        studentFile.WriteLine("Course: " + courseBox.Text + vbTab + "Year/Section: " + yrSecBox.Text)
-        studentFile.WriteLine("Mobile Number: " + mobileNumberBox.Text)
-        studentFile.WriteLine("Birthdate: " + birthdateBox.Text + vbTab + "Gender: " + yrSecBox.Text.ToUpper())
-        studentFile.WriteLine("Address: " + vbNewLine + addressRichBox.Text)
-        studentFile.Close()
+
+        Dim fileName As String
+        fileName = lastNameBox.Text.ToUpper() + ".txt" ' file text creator
+
+
+
+
 
         MessageBox.Show(lastNameBox.Text.ToUpper() + " text file successfully create.", "Information", MessageBoxButtons.OK
                         )
-
+        If lastNameBox.Text <> "" Then
+            If System.IO.File.Exists(fileName) Then
+                MessageBox.Show("file already exist, create another text file.")
+            Else
+                ' file path at \vb_contract_tracing\vb_contract_tracing\bin\Debug\<filename>.txt
+                studentFile = My.Computer.FileSystem.OpenTextFileWriter(fileName, True)
+                studentFile.WriteLine("FullName: " + lastNameBox.Text + ", " + nameBox.Text + " " + miBox.Text)
+                studentFile.WriteLine("Student Number: " + studentNumberBox.Text)
+                studentFile.WriteLine("Course: " + courseBox.Text + vbTab + "Year/Section: " + yrSecBox.Text)
+                studentFile.WriteLine("Mobile Number: " + mobileNumberBox.Text)
+                studentFile.WriteLine("Birthdate: " + birthdateBox.Text + vbTab + "Gender: " + yrSecBox.Text.ToUpper())
+                studentFile.WriteLine("Address: " + vbNewLine + addressRichBox.Text)
+                studentFile.Close()
+            End If
+        Else
+            MessageBox.Show("Fill-up missing form.")
+        End If
 
     End Sub
 End Class
